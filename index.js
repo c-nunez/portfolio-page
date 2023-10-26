84f716cc2196c2bb4469e3a871c5604cd47dfaec
Your Name <nunez_cr@outlook.com>
2023-10-26 00:04:41 -0400

    staging for theme toggler

--- index.js
+++ index.js
@@ -2,3 +2,14 @@
 // $("h2").html("text-light");
 // $("h2").addClass("text-light");
 // $("h2").removeClass("text-light");
+
+// $('#theme-toggler').on('click', function() {
+//     if($('html').attr('data-bs-theme') == 'light'){        
+//         $('html').attr('data-bs-theme', 'dark');
+//         $('#theme-toggler').text('☀️');
+
+//     } else if($('html').attr('data-bs-theme') == 'dark'){
+//         $('html').attr('data-bs-theme', 'light');
+//         $('#theme-toggler').text('🌙');
+//     }
+// });
